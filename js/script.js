117e4406597de6d616406e66b7e597ad07cdb603
241250-Victor_du_Preez <241250@virtualwindow.co.za>
2024-05-22 13:11:57 +0200

    Made disappearing navbar
    Made the navbar disappear when hitting a certain y level when scrolling down.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-//sticky navbar
+//disappearing navbar
+let lastScrollTop = 1; //set starting point
+let navbar = document.getElementById("navbar"); //targets navbar
+let threshold = 750; //set threshold for disappearance
 
-
-
-//contact card
-
-
+if (navbar) { // Check if navbar exists
+    window.addEventListener("scroll", function() {
+        let scrollTop = window.scrollY //get current Y level value
+        
+        if (scrollTop > lastScrollTop && scrollTop > threshold) {
+            navbar.style.top = "-100px"; // Hide Navbar
+        } else {
+            navbar.style.top = "0"; // Show Navbar
+        }
+        lastScrollTop = scrollTop; //updates values based off new Y levels
+    });
+} 
 
 //search bar
 
