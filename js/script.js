0a4aabeb2998a81460fac0d4157acb82bebc595b
241250-Victor_du_Preez <241250@virtualwindow.co.za>
2024-05-22 17:20:43 +0200

    Book Button alert and working counters
    The Book Now button on the featured trips section gives an alert, and all of the counters on the flights page are working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-//disappearing navbar
+//*disappearing navbar*//
+
+
+
 let lastScrollTop = 1; //set starting point
 let navbar = document.getElementById("navbar"); //targets navbar
 let threshold = 750; //set threshold for disappearance
@@ -16,12 +19,57 @@ if (navbar) { // Check if navbar exists
     });
 } 
 
-//search bar
+
+//*item added to cart alert*//
+
+
+    const bookButton = document.getElementById('bookButton'); //targets bookButton
+    bookButton.addEventListener('click', function() { //listens for a click
+        alert('Item Added to Cart!'); //makes the alert pop up when button is clicked
+    });
 
 
 
-//cart counter
+//*counter on flights page*//
+
+
+let counterElements = document.querySelectorAll(".counter"); //select all elements with the class "counter"
+
+counterElements.forEach(counter => { //forEach iterates over each element with the class "counter"
+    let countDisplay = counter.querySelector("#counter-display");
+    let increaseButtons = counter.querySelectorAll("#increase"); //creates variables for the buttons and counter
+    let decreaseButtons = counter.querySelectorAll("#decrease");
+
+  let count = 1; //starting count for each counter
+
+  increaseButtons.forEach(button => {
+    button.addEventListener("click", () => { //listens for when the increase button is pressed
+    if (count < 5) { //sets a max count of 5 per item
+      count++;} //increases the count by 1
+      countDisplay.innerText = count; //displays the current count in the countDisplay
+    });
+  });
+
+  decreaseButtons.forEach(button => {
+    button.addEventListener("click", () => { //listens for when the decrease button is pressed
+      if (count > 1) { //prevents the count being 0, as you cant add 0 things to cart
+        count--;} //decreases the count if its above 0, and if the button has been pressed
+      
+      countDisplay.innerText = count; //displays the current count in the countDisplay
+    });
+  });
+});
 
 
 
-//sort by
+//*search bar*//
+
+
+
+
+
+//*cart counter*//
+
+
+
+//*sort by*//
